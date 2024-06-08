ff8f248282a0418f2d336daa7ed1a81e43ba458b
buckspencer <ad3uck@gmail.com>
2024-06-08 16:51:21 -0700

    Mobile hamburger menu

--- next/src/ui/header/Toggle.tsx
+++ next/src/ui/header/Toggle.tsx
@@ -1,9 +1,23 @@
 export default function Toggle() {
+	const hamburger = (
+		<svg
+			width="24"
+			height="24"
+			viewBox="0 0 24 24"
+			fill="none"
+			xmlns="http://www.w3.org/2000/svg"
+		>
+			<rect x="3" y="6" width="18" height="2" fill="currentColor" />
+			<rect x="3" y="11" width="18" height="2" fill="currentColor" />
+			<rect x="3" y="16" width="18" height="2" fill="currentColor" />
+		</svg>
+	)
+
 	return (
-		<label className="[grid-area:toggle] md:hidden">
+		<label className="pr-10 [grid-area:toggle] md:hidden">
 			<input id="header-open" type="checkbox" hidden />
-			<span className="header-closed:hidden">Close</span>
-			<span className="header-open:hidden">Open</span>
+			<span className="header-closed:hidden">{hamburger}</span>
+			<span className="header-open:hidden">{hamburger}</span>
 		</label>
 	)
 }
