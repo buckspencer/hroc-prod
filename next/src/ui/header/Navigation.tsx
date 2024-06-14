8648ba752872aa5358068df6a4577bd8fa779ff2
buckspencer <ad3uck@gmail.com>
2024-06-13 19:11:02 -0700

    Working tailwind cms components

--- next/src/ui/header/Navigation.tsx
+++ next/src/ui/header/Navigation.tsx
@@ -8,19 +8,19 @@ export default async function Menu() {
 
 	return (
 		<nav className="max-md:anim-fade-to-r text-md flex justify-end gap-y-2 [grid-area:ctas] max-md:my-4 max-md:flex-col max-md:header-closed:hidden">
-			{headerMenu?.items?.map((item, key) => {
-				const isLastItem = key === headerMenu.items.length - 1
-
-				return (
-					<React.Fragment key={key}>
-						{item._type === 'link' && (
-							<CTA className="hover:link text-slate-50 md:px-3" link={item} />
-						)}
-						{item._type === 'link.list' && <LinkList {...item} />}
-						{!isLastItem && <span className="mx-2 text-slate-50">|</span>}
-					</React.Fragment>
-				)
-			})}
-		</nav>
+		{headerMenu?.items?.map((item, key) => {
+			const isLastItem = key === (headerMenu?.items?.length ?? 0) - 1; // Nullish coalescing operator used here
+	
+			return (
+				<React.Fragment key={key}>
+					 {item._type === 'link' && (
+								<CTA className="hover:link text-slate-50 md:px-3" link={item} />
+							)}
+							{item._type === 'link.list' && <LinkList {...item} />}
+							{!isLastItem && <span className="mx-2 text-slate-50">|</span>}
+				</React.Fragment>
+			);
+		})}
+	</nav>
 	)
 }
