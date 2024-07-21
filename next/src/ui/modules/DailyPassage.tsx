0693bc09345daf6f5f53a2dea21cb082e55d288b
buckspencer <ad3uck@gmail.com>
2024-07-21 16:41:23 -0700

    Add synaxarion

--- next/src/ui/modules/DailyPassage.tsx
+++ next/src/ui/modules/DailyPassage.tsx
@@ -16,10 +16,20 @@ interface LiturgicalDay {
   }[];
 }
 
+interface CalendarData {
+  stories: {
+    title: string;
+    story: string;
+  }[];
+}
+
 export default function DailyPassage({ }: Partial<{}>) {
   const [liturgicalDays, setLiturgicalDays] = useState<LiturgicalDay[]>([]);
   const [selectedReading, setSelectedReading] = useState(0); // Default to show the first reading
   const [loading, setLoading] = useState(true); // Loading state
+  const [calendarData, setCalendarData] = useState<CalendarData | null>(null); // Calendar data state
+  const [currentSaintIndex, setCurrentSaintIndex] = useState(0);
+  const [focusSaint, setFocusSaint] = useState(false);
 
   useEffect(() => {
     const fetchLiturgicalData = async () => {
@@ -34,14 +44,42 @@ export default function DailyPassage({ }: Partial<{}>) {
       }
     };
 
+    const fetchCalendarData = async () => {
+      try {
+        const response = await fetch(
+          "https://orthocal.info/api/gregorian/",
+          {
+            next: { revalidate: 3600 },
+            cache: "no-store"
+          }
+        );
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const data = await response.json();
+        setCalendarData(data); // Set the fetched calendar data
+      } catch (error) {
+        console.error("Error fetching calendar data:", error);
+      }
+    };
+
     fetchLiturgicalData();
+    fetchCalendarData();
   }, []); // Empty dependency array ensures this effect runs once on component mount
 
+  // Define the click handler inside the component
+  const handleSaintClick = (index: number) => {
+    setCurrentSaintIndex(index);
+    setFocusSaint(true);
+  };
+
   if (loading) {
-    return <p className="py-6 text-center text-slate-600 text-lg">
-      Requesting readings from Antiochian.org
-      <span className={cn(css.dots)}> ...</span>
-    </p>; // Render loading text
+    return (
+      <p className="py-6 text-center text-slate-600 text-lg">
+        Requesting readings
+        <span className={cn(css.dots)}> ...</span>
+      </p>
+    ); // Render loading text
   }
 
   return (
@@ -92,6 +130,46 @@ export default function DailyPassage({ }: Partial<{}>) {
           </div>
         ))}
       </div>
+      {calendarData && (
+        <div className="mx-auto max-w-7xl md:px-6 lg:px-8">
+          <p className="my-6 text-2xl leading-8 text-slate-700 text-center">- Synaxarion -</p>
+          <div className="mx-auto max-w-2xl lg:mx-0 lg:max-w-lg">
+            <dl className="my-8 max-w-xl space-y-3 text-base leading-7 text-gray-600 lg:max-w-none">
+              {calendarData.stories.map((story, index) => (
+                <div key={index} className="relative">
+                  <dt
+                    className={`block font-semibold hover:text-red-700/80 ${currentSaintIndex === index ? 'text-red-900/80' : 'text-slate-800'}`}
+                    onClick={() => handleSaintClick(index)}
+                    style={{ cursor: 'pointer' }}
+                  >
+                    <a href='#reading-area'>{story.title}</a>
+                  </dt>
+                </div>
+              ))}
+            </dl>
+          </div>
+          <div className="relative py-8 mx-auto sm:rounded-3xl sm:pt-16 min-h-screen">
+            <div className="mb-12 text-base leading-7 text-gray-700 scrollbar w-full">
+              <h1 className="mt-2 mb-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+                {calendarData.stories[currentSaintIndex]?.title}
+              </h1>
+              <div className="prose max-w-none text-slate-900 w-full font-serif">
+                <div dangerouslySetInnerHTML={{ __html: calendarData.stories[currentSaintIndex]?.story }}></div>
+              </div>
+            </div>
+            <div className='text-xs text-center font-serif text-red-900/60'>
+              Daily Reading by
+              <a href={"https://www.antiochian.org/liturgicday"} target='_blank' rel="noreferrer">
+                <p> Antiochian Archdiocese</p>
+              </a>
+              Today's Saints by
+              <a href={"https://orthocal.info/"} target='_blank' rel="noreferrer">
+                <p>Orthocal.info</p>
+              </a>
+            </div>
+          </div>
+        </div>
+      )}
     </>
   );
 }
