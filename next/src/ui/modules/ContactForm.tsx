f3d942335a14ea5f97c2f3ee80643ff03aa9d303
buckspencer <ad3uck@gmail.com>
2024-07-21 19:59:35 -0700

    Updates to working contact form

--- next/src/ui/modules/ContactForm.tsx
+++ next/src/ui/modules/ContactForm.tsx
@@ -1,53 +1,70 @@
-// components/ContactForm.tsx
 "use client";
 
-import { useState } from 'react';
-import { VscMap, VscMail, VscCallOutgoing } from 'react-icons/vsc';
-import { useForm } from 'react-hook-form';
+import React, { useEffect } from "react";
+import { useForm, useWatch } from "react-hook-form";
+import { VscMap, VscMail, VscCallOutgoing } from "react-icons/vsc";
 
-interface FormValues {
-  name: string;
-  email: string;
-  message: string;
-}
+export default function ContactForm() {
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    reset,
+    control,
+    formState: { errors, isSubmitSuccessful, isSubmitting },
+  } = useForm({
+    mode: "onTouched",
+  });
 
-export default function ContactForm({ }: Partial<{}>) {
-  const { register, handleSubmit, formState: { errors, isSubmitting, isSubmitSuccessful }, reset } = useForm<FormValues>();
-  const [isSuccess, setIsSuccess] = useState<boolean>(false);
-  const [message, setMessage] = useState<string>('');
+  const [isSuccess, setIsSuccess] = React.useState(false);
+  const [message, setMessage] = React.useState("");
 
-  const onSubmit = async (data: FormValues) => {
-    try {
-      const response = await fetch('/api/form-submission', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
+  const userName = useWatch({ 
+    control, 
+    name: "name", 
+    defaultValue: "Someone" 
+  });
+  
+  useEffect(() => {
+    setValue('subject', `${userName} sent a message from Website`)
+  }, [userName, setValue]);
+
+  const onSubmit = async (data, e) => {
+    await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({ ...data, access_key: process.env.NEXT_PUBLIC_WEB3FORMS_API_KEY }, null, 2),
+    })
+      .then(async (response) => {
+        let json = await response.json();
+        if (json.success) {
+          setIsSuccess(true);
+          setMessage(json.message);
+          e.target.reset();
+          reset();
+        } else {
+          setIsSuccess(false);
+          setMessage(json.message);
+        }
+      })
+      .catch((error) => {
+        setIsSuccess(false);
+        setMessage("Client Error. Please check the console.log for more info");
+        console.log(error);
       });
-
-      if (!response.ok) {
-        throw new Error('Failed to send message');
-      }
-
-      setIsSuccess(true);
-      setMessage('Success. Message sent successfully');
-      reset(); // Reset form fields
-    } catch (error) {
-      console.error('Error sending message:', error);
-      setIsSuccess(false);
-      setMessage('Something went wrong. Please try again later.');
-    }
   };
 
   return (
     <div className="section flex w-full flex-col">
       <h1 className="mt-2 mb-3 text-3xl font-semibold tracking-tight text-center lg:leading-snug lg:text-4xl dark:text-slate-900">
-      Have a question about our Parish?
+        Have a question about our Parish?
       </h1>
 
-      <div className="grid my-10">
-        <div className="my-10 mx-auto">
+      <div className="grid my-10 md:grid-cols-2">
+        <div className="my-10">
           <h2 className="text-2xl font-semibold dark:text-slate-900">
             Contact
           </h2>
@@ -59,12 +76,12 @@ export default function ContactForm({ }: Partial<{}>) {
           </p>
 
           <div className="mt-5">
-              <a href="https://maps.app.goo.gl/kyDN1bQ4g8ZmRmp3A" target="_blank">
-            <div className="flex items-center mt-2 space-x-2 text-dark-600 dark:text-slate-500">
+            <a href="https://maps.app.goo.gl/kyDN1bQ4g8ZmRmp3A" target="_blank" rel="noopener noreferrer">
+              <div className="flex items-center mt-2 space-x-2 text-dark-600 dark:text-slate-500">
                 <VscMap className="w-4 h-4" />
                 <span>5910 E. 5th Street Tucson, AZ 85711</span>
-            </div>
-              </a>
+              </div>
+            </a>
             <div className="flex items-center mt-2 space-x-2 text-dark-600 dark:text-slate-500">
               <VscMail className="w-4 h-4" />
               <a href="mailto:admin@holyresurrectiontucson.org">admin@holyresurrectiontucson.org</a>
@@ -75,8 +92,115 @@ export default function ContactForm({ }: Partial<{}>) {
             </div>
           </div>
         </div>
-        
+        <div>
+          <form onSubmit={handleSubmit(onSubmit)} className="my-10">
+            <div className="mb-5">
+              <input
+                type="text"
+                placeholder="Full Name"
+                autoComplete="false"
+                className={`w-full px-4 py-3 border-2 placeholder:text-slate-500 dark:text-slate-900 rounded-md outline-none dark:placeholder:text-slate-700 dark:bg-yellow-600/50 focus:ring-4 ${
+                  errors.name
+                    ? "border-red-600 focus:border-red-600 ring-red-100 dark:ring-0"
+                    : "border-slate-600 focus:border-gray-600 ring-gray-100 dark:border-gray-600 dark:focus:border-white dark:ring-0"
+                }`}
+                {...register("name", {
+                  required: "Full name is required",
+                  maxLength: 80
+                })}
+              />
+              {errors.name && (
+                <div className="mt-1 text-red-600">
+                  <small>{errors.name.message}</small>
+                </div>
+              )}
+            </div>
+
+            <div className="mb-5">
+              <label htmlFor="email_address" className="sr-only">
+                Email Address
+              </label>
+              <input
+                id="email_address"
+                type="email"
+                placeholder="Email Address"
+                autoComplete="false"
+                className={`w-full px-4 py-3 border-2 placeholder:text-slate-500 dark:text-slate-900 rounded-md outline-none dark:placeholder:text-slate-700 dark:bg-yellow-600/50 focus:ring-4 ${
+                  errors.email
+                    ? "border-red-600 focus:border-red-600 ring-red-100 dark:ring-0"
+                    : "border-slate-600 focus:border-gray-600 ring-gray-100 dark:border-gray-600 dark:focus:border-white dark:ring-0"
+                }`}
+                {...register("email", {
+                  required: "Enter your email",
+                  pattern: {
+                    value: /^\S+@\S+$/i,
+                    message: "Please enter a valid email"
+                  }
+                })}
+              />
+              {errors.email && (
+                <div className="mt-1 text-red-600">
+                  <small>{errors.email.message}</small>
+                </div>
+              )}
+            </div>
+
+            <div className="mb-3">
+              <textarea
+                id="message"
+                placeholder="Your Message"
+                className={`w-full px-4 py-3 border-2 placeholder:text-slate-500 dark:text-slate-900 dark:placeholder:text-slate-700 dark:bg-yellow-600/50 rounded-md outline-none h-36 focus:ring-4 ${
+                  errors.message
+                    ? "border-red-600 focus:border-red-600 ring-red-100 dark:ring-0"
+                    : "border-slate-600 focus:border-gray-600 ring-gray-100 dark:border-gray-600 dark:focus:border-white dark:ring-0"
+                }`}
+                {...register("message", {
+                  required: "Enter your Message"
+                })}
+              />
+              {errors.message && (
+                <div className="mt-1 text-red-600">
+                  <small>{errors.message.message}</small>
+                </div>
+              )}
+            </div>
+
+            <button
+              type="submit"
+              className="w-full py-4 font-semibold text-slate-900 transition-colors bg-slate-300 rounded-md hover:bg-yellow-600/20 focus:outline-none focus:ring-offset-2 focus:ring focus:ring-gray-200 px-7 dark:bg-yellow-600 dark:text-black">
+              {isSubmitting ? (
+                <svg
+                  className="w-5 h-5 mx-auto text-slate-900/60 dark:text-black animate-spin"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24">
+                  <circle
+                    className="opacity-25"
+                    cx="12"
+                    cy="12"
+                    r="10"
+                    stroke="currentColor"
+                    strokeWidth="4"></circle>
+                  <path
+                    className="opacity-75"
+                    fill="currentColor"
+                    d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                </svg>
+              ) : (
+                "Send Message"
+              )}
+            </button>
+          </form>
+
+          {isSubmitSuccessful && (
+            <div className="mt-3 text-sm text-center">
+              <span className={isSuccess ? 'text-green-500' : 'text-red-500'}>
+                {message}
+              </span>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
-};
+}
