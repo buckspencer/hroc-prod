0dd73d25226038095607071f196afbfa2da3bbe3
buckspencer <ad3uck@gmail.com>
2024-08-03 16:59:04 -0700

    Update  * Padding left to mobile linklist

--- next/src/ui/header/LinkList.tsx
+++ next/src/ui/header/LinkList.tsx
@@ -6,12 +6,12 @@ import { CgChevronRight } from 'react-icons/cg'
 export default function LinkList({ label, links }: Sanity.LinkList) {
 	return (
 		<InteractiveDetails className="group relative" closeAfterNavigate>
-			<summary className="flex items-center gap-1 text-slate-50 md:px-3">
+			<summary className="flex items-center gap-1 text-slate-50 md:px-3 pl-2 md:pl-0">
 				{label}
 				<CgChevronRight className="transition-transform group-open:rotate-90 md:rotate-90" />
 			</summary>
 
-			<ul className="anim-fade-to-b md:frosted-glass left-0 top-full px-3 py-2 max-md:border-l md:absolute md:min-w-max md:rounded md:border md:bg-canvas md:shadow-md">
+			<ul className="text-slate-50 anim-fade-to-b md:frosted-glass left-0 top-full px-3 py-2 max-md:border-l md:absolute md:min-w-max md:rounded md:border md:bg-canvas md:shadow-md">
 				{links?.map((link, key) => (
 					<li key={key}>
 						<CTA
--- next/src/ui/header/Navigation.tsx
+++ next/src/ui/header/Navigation.tsx
@@ -1,26 +1,26 @@
-import { getSite } from '@/lib/sanity/queries'
-import CTA from '@/ui/CTA'
-import LinkList from './LinkList'
-import React from 'react'
+import { getSite } from '@/lib/sanity/queries';
+import CTA from '@/ui/CTA';
+import LinkList from './LinkList';
+import React from 'react';
 
 export default async function Menu() {
-	const { headerMenu } = await getSite()
+  const { headerMenu } = await getSite();
 
-	return (
-		<nav className="max-md:anim-fade-to-r text-sm flex justify-end gap-y-2 [grid-area:ctas] max-md:my-4 max-md:flex-col max-md:header-closed:hidden">
-		{headerMenu?.items?.map((item, key) => {
-			const isLastItem = key === (headerMenu?.items?.length ?? 0) - 1; // Nullish coalescing operator used here
-	
-			return (
-				<React.Fragment key={key}>
-					 {item._type === 'link' && (
-								<CTA className="hover:link md:text-center align-top text-slate-50 md:px-3" link={item} />
-							)}
-							{item._type === 'link.list' && <LinkList {...item} />}
-							{!isLastItem && <span className="hidden lg:inline mx-2 text-slate-50">|</span>}
-				</React.Fragment>
-			);
-		})}
-	</nav>
-	)
+  return (
+    <nav className="max-md:anim-fade-to-r text-sm flex justify-end gap-y-2 [grid-area:ctas] max-md:my-4 max-md:flex-col max-md:header-closed:hidden">
+      {headerMenu?.items?.map((item, key) => {
+        const isLastItem = key === (headerMenu?.items?.length ?? 0) - 1;
+
+        return (
+          <React.Fragment key={key}>
+            {item._type === 'link' && (
+              <CTA className="hover:link md:text-center align-top text-slate-50 md:px-3 pl-2 md:pl-0" link={item} />
+            )}
+            {item._type === 'link.list' && <LinkList {...item} />}
+            {!isLastItem && <span className="hidden lg:inline mx-2 text-slate-50">|</span>}
+          </React.Fragment>
+        );
+      })}
+    </nav>
+  );
 }
