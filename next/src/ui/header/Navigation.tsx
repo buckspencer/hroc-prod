556d2514da54b1eb6cf2acee1ced60bee9998fcd
buckspencer <ad3uck@gmail.com>
2024-09-28 12:43:53 -0700

    Remove strange gradient in navbar.

--- next/src/ui/header/Navigation.tsx
+++ next/src/ui/header/Navigation.tsx
@@ -7,7 +7,7 @@ export default async function Menu() {
   const { headerMenu } = await getSite();
 
   return (
-    <nav className="max-md:anim-fade-to-r text-sm flex justify-end gap-y-2 [grid-area:ctas] max-md:my-4 max-md:flex-col max-md:header-closed:hidden">
+    <nav className="max-md text-sm flex justify-end gap-y-2 [grid-area:ctas] max-md:my-4 max-md:flex-col max-md:header-closed:hidden">
       {headerMenu?.items?.map((item, key) => {
         const isLastItem = key === (headerMenu?.items?.length ?? 0) - 1;
 
