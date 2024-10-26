12245d14ca323ed3a2f35292a51c623ccaa5129c
buckspencer <ad3uck@gmail.com>
2024-10-25 22:35:03 -0700

    Fix nav dropdown color

--- next/src/ui/header/LinkList.tsx
+++ next/src/ui/header/LinkList.tsx
@@ -11,7 +11,7 @@ export default function LinkList({ label, links }: Sanity.LinkList) {
 				<CgChevronRight className="transition-transform group-open:rotate-90 md:rotate-90" />
 			</summary>
 
-			<ul className="text-slate-50 anim-fade-to-b md:frosted-glass left-0 top-full px-3 py-2 max-md:border-l md:absolute md:min-w-max md:rounded md:border md:bg-canvas md:shadow-md">
+			<ul className="text-slate-800 left-0 top-full px-3 py-2 max-md:border-l md:absolute md:min-w-max md:rounded md:border md:bg-canvas md:shadow-md">
 				{links?.map((link, key) => (
 					<li key={key}>
 						<CTA
