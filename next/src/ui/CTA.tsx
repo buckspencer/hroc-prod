5bf43740a77118481fa8b7a49d8eaa280987e08f
buckspencer <ad3uck@gmail.com>
2024-10-26 11:20:47 -0700

    UPdate external CTA links are target blank

--- next/src/ui/CTA.tsx
+++ next/src/ui/CTA.tsx
@@ -1,6 +1,6 @@
-import Link from 'next/link'
-import { cn } from '@/lib/utils'
-import processUrl from '@/lib/processUrl'
+import Link from 'next/link';
+import { cn } from '@/lib/utils';
+import processUrl from '@/lib/processUrl';
 
 export default function CTA({
 	link,
@@ -8,28 +8,35 @@ export default function CTA({
 	className,
 	children,
 }: Sanity.CTA & React.HTMLAttributes<HTMLAnchorElement>) {
-	if (!link?.type) return null
+	if (!link?.type) return null;
 
 	const props = {
 		className: cn(style, className),
 		children: children || link.label || link.internal?.title,
-	}
+	};
 
 	switch (link.type) {
 		case 'internal':
-			if (!link.internal) return null
+			if (!link.internal) return null;
 
 			return (
 				<Link
 					href={processUrl(link.internal, { base: false, params: link.params })}
 					{...props}
 				/>
-			)
+			);
 
 		case 'external':
-			return <a href={link.external} {...props} />
+			return (
+				<a
+					href={link.external}
+					target="_blank"
+					rel="noreferrer"
+					{...props}
+				/>
+			);
 
 		default:
-			return null
+			return null;
 	}
 }
